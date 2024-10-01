82c2ca74ac638fac280652619c75e19ab5da8f21
Robiully127 <rislamsohid@gmail.com>
2024-10-01 14:18:03 +0600

    Create Song.js

---
+++ scripts/commands/Song.js
@@ -0,0 +1,57 @@
+module.exports.config = {
+  name: "song",
+  version: "1.0.0",
+  permission: 0,
+  credits: "SIDDIK",
+  description: "",
+  prefix: true, 
+  category: "no prefix", 
+  usages: "user",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+ 
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  if (event.body.indexOf("song") == 0) {
+    const axios = global.nodemodule["axios"];
+    const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    var link = ["https://i.imgur.com/pWPvn4n.mp4"];
+    var callback = () => api.sendMessage({
+      body: `Lofi Song`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    const timeStart = Date.now();
+    const PREFIX = config.PREFIX;
+    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
+  }
+};
+ 
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+};
+ 
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  let { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["song"] == "undefined" || data["song"] == true) data["song"] = false;
+  else data["song"] = true;
+  await Threads.setData(threadID, {
+    data
+  });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["song"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
