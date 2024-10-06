dee0409f4c26f0823659f194b81655d499609e2f
Robiully127 <rislamsohid@gmail.com>
2024-10-06 15:21:39 +0600

    Update npx2.js

--- scripts/commands/npx2.js
+++ scripts/commands/npx2.js
@@ -19,7 +19,6 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/cgoacAc.mp4',
       "https://i.imgur.com/BcDJZte.mp4",
       { responseType: 'stream' }
     )
